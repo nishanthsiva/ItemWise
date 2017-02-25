d53c8a033423b0ffa2e56592ccae7c9ca39959e9
ananthrkn <ananth@iastate.edu>
2017-02-24 22:37:18 -0600

    Login Activity

--- app/src/main/java/com/wolfsoft/teammeetingschedule/MainActivity.java
+++ app/src/main/java/com/wolfsoft/teammeetingschedule/MainActivity.java
@@ -1,13 +1,37 @@
 package com.wolfsoft.teammeetingschedule;
 
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button button;
+    EditText username, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        button = (Button) findViewById(R.id.submit);
+
+        button.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent redirect = new Intent(this, Home.class);
+        startActivity(redirect);
     }
 }
