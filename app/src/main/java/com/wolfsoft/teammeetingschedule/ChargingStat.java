5f4467b5e05d5535dd26919cd171f8cb0474d875
Vishal T Vijay <Vishal T Vijay>
2017-02-25 22:09:57 -0600

    Battery charging stats

---
+++ app/src/main/java/com/wolfsoft/teammeetingschedule/ChargingStat.java
@@ -0,0 +1,60 @@
+package com.wolfsoft.teammeetingschedule;
+
+import android.database.sqlite.SQLiteDatabase;
+
+import com.wolfsoft.teammeetingschedule.utilities.BatteryChargeDAO;
+import com.wolfsoft.teammeetingschedule.utilities.DatabaseHelper;
+
+/**
+ * Created by visha on 25-Feb-17.
+ */
+
+public class ChargingStat {
+
+
+    public float ImportStat(int stat, SQLiteDatabase db)
+    {
+        int i=0,dc=0,rc=0;
+        float discharge_r=0,charge_r=0;
+        long start_t=0,stop_t=0;
+        int start_c = 0, stop_c = 0;
+
+        BatteryChargeDAO BatteryStat= new BatteryChargeDAO(db);
+        String StatData[] =BatteryStat.getData();
+
+        while(i<StatData.length){
+            start_t=Long.parseLong(StatData[i]);
+            i++;
+            start_c=Integer.parseInt(StatData[i]);
+            i++;
+            if((i+2)<StatData.length ){
+                if(i>1){
+                    discharge_r=discharge_r+ChargeChange(stop_t,stop_c,start_t,start_c);
+                    dc++;
+                }
+                stop_t= Long.parseLong(StatData[i]);
+                i++;
+                stop_c=Integer.parseInt(StatData[i]);
+                i++;
+                charge_r=charge_r+ChargeChange(start_t,start_c,stop_t,stop_c);
+                rc++;
+            }else{
+                discharge_r=discharge_r+ChargeChange(stop_t,stop_c,start_t,start_c);
+                dc++;
+            }
+        }
+        if(stat==0) {
+            discharge_r = discharge_r / dc;
+            return discharge_r;
+        }else{
+        charge_r=charge_r/rc;
+        return charge_r;
+        }
+
+    }
+    public float ChargeChange(long start_t,int start_c,long stop_t, int stop_c){
+        float rate;
+        rate=(start_c-stop_c)/(start_t-stop_t);
+    return rate;
+    }
+}
--- app/src/main/java/com/wolfsoft/teammeetingschedule/utilities/BatteryChargeDAO.java
+++ app/src/main/java/com/wolfsoft/teammeetingschedule/utilities/BatteryChargeDAO.java
@@ -1,10 +1,17 @@
 package com.wolfsoft.teammeetingschedule.utilities;
 
+import android.app.usage.UsageEvents;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.icu.text.SimpleDateFormat;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by vijay on 25-02-2017.
@@ -36,6 +43,42 @@ public class BatteryChargeDAO {
         values.put("STOP_TIME",dateFormat.format(date.getTime()));
 
         long newRowId = db.update("BATTERY_CHARGE",values,"B_ID = ?",new String[]{""+rowID});
+    }
+
+
+    public String[] getData(){
+
+        int charge;
+
+        String[] projection = {"START_TIME","START_CHARGE","STOP_TIME","STOP_CHARGE"};
+
+        Cursor c = db.query("START_TIME",projection,null,null,null,null,null,null);
+
+        boolean stat = c.moveToFirst();
+
+        List<String> data = new ArrayList<>();
+
+        if (c.moveToFirst()) {
+            try {
+            do {
+
+                    String start_t = dateFormat.parse(c.getString(0)).getTime()+"";
+                    String start_c  = c.getString(1);
+                    String stop_t = dateFormat.parse(c.getString(2)).getTime()+"";
+                    String stop_c = c.getString(3);
+                    data.add(start_t);
+                    data.add(start_c);
+                    data.add(stop_t);
+                    data.add(stop_c);
+
+            } while (c.moveToNext());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+        c.close();
+        return data.toArray(new String[0]);
+
     }
 
 }
